8f2a58e7120624da7d71a60f018329a72cde0f22
joaogdcarvalho <joaogabrieldecarvalho.com@gmail.com>
2024-02-26 19:22:02 -0300

    refactor(src): Atualiza estrutura do projeto para atender histórias de usuário.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const navbarLinks = document.querySelectorAll('.nav-link');
+const projectName = "pagina-documentacao-tecnica";
 
-  navbarLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarLinks = document.querySelectorAll(".nav-link");
+
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
+      const targetId = this.getAttribute("href").substring(1);
       const targetSection = document.getElementById(targetId);
-      targetSection.scrollIntoView({ behavior: 'smooth' });
+      targetSection.scrollIntoView({ behavior: "smooth" });
     });
   });
 });
